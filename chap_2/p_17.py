9af45d014e163e457327466dc50d94fec5f48a8a
ReoChiyomaru <reo.chiyomaru@gmail.com>
2024-04-07 23:35:25 +0900

    output to unique_strings.txt

--- chap_2/p_17.py
+++ chap_2/p_17.py
@@ -1,6 +1,8 @@
-with open('popular-names.txt', 'r') as f:
+with open('popular-names.txt', 'r') as f, open('unique_strings.txt', 'w') as out_file:
     lines = f.readlines()
-unique_strings = set(line.split()[0] for line in lines)
-print(unique_strings)
+    unique_strings = set(line.split()[0] for line in lines)
+    for string in unique_strings:
+        out_file.write(string + '\n')
+    print(unique_strings)
 
-# 確認コマンド：cut -f 1 popular-names.txt | sort | uniq
+# 確認コマンド：cut -f 1 popular-names.txt | sort | uniq > unique_strings.txt
