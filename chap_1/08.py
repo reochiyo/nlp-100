419376037f10ec8831f462ccbe2ae275469bad24
ReoChiyomaru <reo.chiyomaru@gmail.com>
2024-04-07 19:56:59 +0900

    Way2を追加

--- chap_1/08.py
+++ chap_1/08.py
@@ -1,3 +1,4 @@
+#Way1
 def cipher(text):
     return ''.join([chr(219 - ord(c)) if c.islower() else c for c in text])
 
@@ -6,3 +7,18 @@ ans = cipher(str)
 ans_back = cipher(ans)
 print(ans)
 print(ans_back)
+
+#Way2
+def cipher2(text):
+    result = ''
+    for c in text:
+        if c.islower():
+            result += chr(219 - ord(c))
+        else:
+            result += c
+    return result
+
+ans2 = cipher2(str)
+ans2_back = cipher2(ans2)
+print(ans2)
+print(ans2_back)
