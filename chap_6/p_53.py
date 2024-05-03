7fd44e9d980827322c1b0152667d2a0d2ffdedfc
ReoChiyomaru <reo.chiyomaru@gmail.com>
2024-05-04 03:01:40 +0900

    change to dataframe

--- chap_6/p_53.py
+++ chap_6/p_53.py
@@ -22,8 +22,11 @@ example_headline = "Apple launches new product"
 #記事の見出しを特徴量に変換
 x = vectorizer.transform([example_headline])
 
+#引数の方を変換
+x_data = pd.DataFrame(x)
+
 #ロジスティック回帰モデルでカテゴリを予測
-predicted_category = model.predict(x)
+predicted_category = model.predict(x_data)
 
 #予想確率の計算
 predicted_probability = model.predict_proba(x)
